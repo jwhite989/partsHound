1584b0fdadf084f577ae55860c5569c15f1f594f
Justin White <j.white0513@gmail.com>
2025-12-01 16:18:29 -0500

    added POST controller/route to create part

--- server/controllers/partController.js
+++ server/controllers/partController.js
@@ -23,3 +23,16 @@ export const getSinglePart = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const createPart = async (req, res) => {
+  try {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(400).json({ message: "Request body cannot be empty" });
+    }
+
+    const newPart = await Part.create(req.body);
+    res.status(201).json(newPart);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- server/routes/partRoutes.js
+++ server/routes/partRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getAllParts, getSinglePart } from "../controllers/partController.js";
+import {
+  getAllParts,
+  getSinglePart,
+  createPart,
+} from "../controllers/partController.js";
 
 const router = Router();
 
 router.get("/", getAllParts);
 router.get("/:id", getSinglePart);
+router.post("/", createPart);
 
 export default router;
